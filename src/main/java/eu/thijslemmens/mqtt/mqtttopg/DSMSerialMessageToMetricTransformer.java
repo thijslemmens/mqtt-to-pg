a1b97b285528085b0fc3e379496eda21fe9045dc
Thijs Lemmens <thijs.lemmens@xenit.eu>
2022-02-12 14:47:34 +0100

    add voltage and current, correct some namings

--- src/main/java/eu/thijslemmens/mqtt/mqtttopg/DSMSerialMessageToMetricTransformer.java
+++ src/main/java/eu/thijslemmens/mqtt/mqtttopg/DSMSerialMessageToMetricTransformer.java
@@ -24,6 +24,8 @@ public class DSMSerialMessageToMetricTransformer implements GenericTransformer<M
     private static final DateTimeFormatter TIMESTAMP_FORMAT = DateTimeFormatter.ofPattern("yyMMddHHmmss");
     private static final Pattern GAS_METER_PATTERN = Pattern.compile(
             "\\{\\\"SerialReceived\\\":\\\"0-1:24\\.2\\.3\\((?<timestamp>\\d{12})W\\)\\((?<volume>\\d{5}\\.\\d{3})\\*m3\\)\\\\r\\\"\\}");
+    private static final Pattern ELECTRICITY_VOLTAGE_PATTERN = Pattern.compile("\\{\\\"SerialReceived\\\":\\\"1-0:32.7.0\\((?<voltage>\\d{3}\\.\\d)\\*V\\)\\\\r\\\"\\}");
+    private static final Pattern ELECTRICITY_CURRENT_PATTERN = Pattern.compile("\\{\\\"SerialReceived\\\":\\\"1-0:31.7.0\\((?<current>\\d{3}\\.\\d{2})\\*A\\)\\\\r\\\"\\}");
 
     private LocalDateTime timestamp = null;
 
@@ -45,7 +47,7 @@ public class DSMSerialMessageToMetricTransformer implements GenericTransformer<M
             String volume = gasMeterMatcher.group("volume");
             logger.debug("Gasmeter at {}: {}m3", gasTimestamp, volume);
             return Optional.of(
-                    new Metric("gasMeter", Double.parseDouble(volume), stringToLocalDateTime(gasTimestamp), Map.of(),
+                    new Metric("gas.meter", Double.parseDouble(volume), stringToLocalDateTime(gasTimestamp), Map.of(),
                             "m3"));
         }
 
@@ -59,7 +61,7 @@ public class DSMSerialMessageToMetricTransformer implements GenericTransformer<M
             String tariff = electricityMeterMatcher.group("tariff");
             String value = electricityMeterMatcher.group("value");
             logger.debug(String.format("Direction: %s, Tariff: %s, Value: %s kWh", direction, tariff, value));
-            return Optional.of(new Metric("electricityMeter", Double.parseDouble(value), timestamp,
+            return Optional.of(new Metric("electricity.meter", Double.parseDouble(value), timestamp,
                     Map.of("direction", mapDirection(direction), "tariff", mapTariff(tariff)), "kWh"));
         }
 
@@ -68,8 +70,22 @@ public class DSMSerialMessageToMetricTransformer implements GenericTransformer<M
             String direction = powerMatcher.group("direction");
             String power = powerMatcher.group("value");
             logger.debug(String.format("Direction: %s, Value %s kW", direction, power));
-            return Optional.of(new Metric("electricityPower", Double.valueOf(power), timestamp,
+            return Optional.of(new Metric("electricity.power", Double.valueOf(power), timestamp,
                     Map.of("direction", mapDirection(direction)), "kW"));
+        }
+
+        Matcher voltageMatcher = ELECTRICITY_VOLTAGE_PATTERN.matcher(payload);
+        if (voltageMatcher.find()) {
+            String voltage = powerMatcher.group("voltage");
+            logger.debug("Voltage: {}", voltage);
+            return Optional.of(new Metric("electricity.voltage", Double.parseDouble(voltage), timestamp, Map.of(), "V"));
+        }
+
+        Matcher currentMatcher = ELECTRICITY_CURRENT_PATTERN.matcher(payload);
+        if (currentMatcher.find()) {
+            String current = currentMatcher.group("current");
+            logger.debug("Current: {}", current);
+            return Optional.of(new Metric("electricity.current", Double.parseDouble(current), timestamp, Map.of(), "A"));
         }
 
         return Optional.empty();
@@ -93,9 +109,9 @@ public class DSMSerialMessageToMetricTransformer implements GenericTransformer<M
     private static String mapTariff(String match) {
         switch (match) {
             case "1":
-                return "day";
+                return "high";
             case "2":
-                return "night";
+                return "low";
             default:
                 return match;
         }
