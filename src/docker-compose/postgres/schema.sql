4d44c3e0ae8729390a98efc78f8fa89bbf900b9e
Thijs Lemmens <thijs.lemmens@xenit.eu>
2022-02-27 19:21:10 +0100

    Add continuous aggregates for gas and electricity

--- src/docker-compose/postgres/schema.sql
+++ src/docker-compose/postgres/schema.sql
@@ -16,3 +16,44 @@ create index metrics_labels on metrics using gin (labels);
 create index metrics_unit on metrics using btree (unit);
 
 SELECT create_hypertable('metrics', 'time');
+
+DROP MATERIALIZED VIEW electricity_1min;
+CREATE MATERIALIZED VIEW electricity_1min
+            WITH (timescaledb.continuous)
+AS
+SELECT
+    time_bucket('1 minute', time) AS time_1min,
+    round((max(case when (labels->'tariff'='high' and labels->'direction'='in') then value else 0 end) + max(case when (labels->'tariff'='low' and labels->'direction'='in') then value else 0 end))::numeric,3) AS meter_in,
+    round((max(case when (labels->'tariff'='high' and labels->'direction'='out') then value else 0 end) + max(case when (labels->'tariff'='low' and labels->'direction'='out') then value else 0 end))::numeric,3) AS meter_out
+FROM
+    metrics
+WHERE
+        name = 'electricity.meter'
+GROUP BY
+    time_1min
+WITH NO DATA;
+
+SELECT add_continuous_aggregate_policy('electricity_1min',
+                                       start_offset => INTERVAL '10 minutes',
+                                       end_offset => INTERVAL '2 minute',
+                                       schedule_interval => INTERVAL '10 minutes');
+
+DROP MATERIALIZED VIEW gas_5min;
+CREATE MATERIALIZED VIEW gas_5min
+            WITH (timescaledb.continuous)
+AS
+SELECT
+    time_bucket('5 minute', time) AS time_5min,
+    max(value) as gasMeter
+FROM
+    metrics
+WHERE
+        name = 'gas.meter'
+GROUP BY
+    time_5min
+WITH NO DATA;
+
+SELECT add_continuous_aggregate_policy('gas_5min',
+                                       start_offset => INTERVAL '1 hour',
+                                       end_offset => INTERVAL '30 minutes',
+                                       schedule_interval => INTERVAL '10 minutes');
